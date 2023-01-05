e344a7e41f7b65ea138c0ac77866121d3becec21
g-kassis <121769292+g-kassis@users.noreply.github.com>
2023-01-05 17:52:10 -0500

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -20,17 +20,23 @@ function scrollTop(){
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
+function sendMail(){
+  window.open('mailto:giovannikassis@hotmail.com', '_blank').focus();
+}
+
 document.addEventListener('DOMContentLoaded', function() {
 
   //Home in navbar to scrollto the top
   document.getElementById('home').addEventListener('click', scrollTop)
 
-  //user login and sign up
+  //button handlers
   document.getElementById('githubButton').addEventListener('click', github)
   document.getElementById('githubButton2').addEventListener('click', github)
-  document.getElementById('linkedButton').addEventListener('click', linkedin)
+  document.getElementById('sendMail').addEventListener('click', sendMail)
+  document.getElementById('sendMail2').addEventListener('click', sendMail)
+  document.getElementById('linkedinButton').addEventListener('click', linkedin)
+  document.getElementById('linkedinButton2').addEventListener('click', linkedin)
 
-  //add key handler for the document as a whole, not separate elements.
   document.addEventListener('keyup', handleKeyUp)
 
 })
